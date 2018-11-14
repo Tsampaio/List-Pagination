1bb39f17f99b292dc182f7344880ab7b6379971f
Telmo Sampaio <telmoasampaio@gmail.com>
2018-11-14 21:26:41 +0000

    filtered students based on page number

--- js/script.js
+++ js/script.js
@@ -17,24 +17,22 @@ FSJS project 2 - List Filter and Pagination
    scoped to that function.
 ***/
 
+const students = document.querySelectorAll(".student-item");
+const maxStudents = 10;
+let pageNumber = 1;
 
+const showPage = (list, page) => {
+  
+   for (let i = 0; i < students.length; i++ ) {
+      if( i >= (page - 1) * maxStudents && i < (page * maxStudents)) {
+         list[i].style.display = "";
+      } else {
+         list[i].style.display = "none";
+      }
+   }
 
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-
+}
+showPage(students, 1);
 
 
 
